333ba9f21ee93e6036776799602e32b15079681b
Mokesov Eldar <eldarmol587@gmail.com>
2020-11-09 17:28:36 +0300

    Все удалил

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from './components/header/header';
+import Header from './components/header';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
     </div>
   );
-}
+};
 
 export default App;
