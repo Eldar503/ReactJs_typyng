4a5136cd4952baae82514a6c0549d606b86c1181
Mokesov Eldar <eldarmol587@gmail.com>
2020-11-09 16:55:18 +0300

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,23 @@
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container">
+                <div className="header__top"></div>
+                <div className="header__bottom"></div>
+                <nav className="header__nav">
+                    <ul className="nav__menu">
+                        <li className="header__item"><a href="#" className="nav__link"></a></li>
+                        <li className="header__item"><a href="#" className="nav__link"></a></li>
+                        <li className="header__item"><a href="#" className="nav__link"></a></li>
+                        <li className="header__item"><a href="#" className="nav__link"></a></li>
+                        <li className="header__item"><a href="#" className="nav__link"></a></li>
+                        <li className="header__item"><a href="#" className="nav__link"></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
