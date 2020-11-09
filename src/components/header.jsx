f6fa4732f75350aad7dee0541a395f0a08d81dee
Mokesov Eldar <eldarmol587@gmail.com>
2020-11-09 16:55:59 +0300

    Добавил текст

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
                 <div className="header__bottom"></div>
                 <nav className="header__nav">
                     <ul className="nav__menu">
-                        <li className="header__item"><a href="#" className="nav__link"></a></li>
-                        <li className="header__item"><a href="#" className="nav__link"></a></li>
+                        <li className="header__item"><a href="#" className="nav__link">Home</a></li>
+                        <li className="header__item"><a href="#" className="nav__link">New In</a></li>
                         <li className="header__item"><a href="#" className="nav__link"></a></li>
                         <li className="header__item"><a href="#" className="nav__link"></a></li>
                         <li className="header__item"><a href="#" className="nav__link"></a></li>
