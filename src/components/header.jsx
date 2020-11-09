522e7021c7dca2098b905f8d6835c4e05d308b19
Mokesov Eldar <eldarmol587@gmail.com>
2020-11-09 16:57:40 +0300

    Commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
                     <ul className="nav__menu">
                         <li className="header__item"><a href="#" className="nav__link">Home</a></li>
                         <li className="header__item"><a href="#" className="nav__link">New In</a></li>
-                        <li className="header__item"><a href="#" className="nav__link"></a></li>
+                        <li className="header__item"><a href="#" className="nav__link">Women Product</a></li>
                         <li className="header__item"><a href="#" className="nav__link"></a></li>
                         <li className="header__item"><a href="#" className="nav__link"></a></li>
                         <li className="header__item"><a href="#" className="nav__link"></a></li>
