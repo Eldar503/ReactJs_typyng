f766fc7b81b1e7db1afdc16c8cf0511110835313
Mokesov Eldar <eldarmol587@gmail.com>
2020-11-09 16:59:50 +0300

    Menu burger

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,9 +10,9 @@ const Header = () => {
                         <li className="header__item"><a href="#" className="nav__link">Home</a></li>
                         <li className="header__item"><a href="#" className="nav__link">New In</a></li>
                         <li className="header__item"><a href="#" className="nav__link">Women Product</a></li>
-                        <li className="header__item"><a href="#" className="nav__link"></a></li>
-                        <li className="header__item"><a href="#" className="nav__link"></a></li>
-                        <li className="header__item"><a href="#" className="nav__link"></a></li>
+                        <li className="header__item"><a href="#" className="nav__link">Men Product</a></li>
+                        <li className="header__item"><a href="#" className="nav__link">Top Brands</a></li>
+                        <li className="header__item"><a href="#" className="nav__link">Special Offers</a></li>
                     </ul>
                 </nav>
             </div>
