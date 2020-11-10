812062030dc339d3d28d84190be574baa959388b
Mokesov Eldar <eldarmol587@gmail.com>
2020-11-10 16:49:14 +0300

    Hero

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 
-function hero() {
+function Hero = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="hero">
+            <div className="container">
+                <div className="hero__content">
+                    <div className="hero__product">
+                        <h1 className="hero__title">
+
+                        </h1>
+                        <h3 className="hero__suptitle">
+
+                        </h3>
+                        <p className="hero__description">
+
+                        </p>
+                        <span className="hero__price">
+
+                        </span>
+                        <a href="" className="hero__btn">
+                            
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
-export default hero
+export default  Hero; 
